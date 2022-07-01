88d845326fed670a90f7c9d03dc76804f8b01790
tvhkhang <84630541+tvhkhang@users.noreply.github.com>
2022-07-01 16:15:55 +0700

    Revert "fix"
    This reverts commit 10670d2fda86206f85883442f0fb9202fcfa1452.

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,26 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:new_hotel_app/ui/modules/route_name.dart';
 import 'package:new_hotel_app/ui/modules/router.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 bool shouldUseFirestoreEmulator = false;
-
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   FirebaseAuth.instance.signOut();
   runApp(const MyApp());
+  FlutterNativeSplash.remove();
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyApp();
-}
-
-class _MyApp extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(

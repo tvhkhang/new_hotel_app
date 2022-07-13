122b1d3831bdeede65ec72aa6491b9955e007f86
tvhkhang <84630541+tvhkhang@users.noreply.github.com>
2022-07-13 14:21:18 +0700

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,39 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:new_hotel_app/ui/modules/route_name.dart';
 import 'package:new_hotel_app/ui/modules/router.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'firebase_options.dart';
 
 bool shouldUseFirestoreEmulator = false;
+
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  if (!kIsWeb) FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   FirebaseAuth.instance.signOut();
   runApp(const MyApp());
-  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    var platform = Theme.of(context).platform;
+    if (!kIsWeb) {
+      FlutterNativeSplash.remove();
+    }
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: RouteName.draft,
+
+      initialRoute: RouteName.loginPage,
       onGenerateRoute: RouterK.generateRoute,
     );
   }
